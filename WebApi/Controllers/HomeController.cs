c375288b6a1cb4658131974b483032eb26583f5c
WILMER STIVEN LEON SUACHE <stivenleon1997@gmail.com>
2021-09-21 11:57:58 -0500

    SL- TOKEN API

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("~/API/Home/PostCar")]
+        public string PostCar()
+        {
+            return "HOLA";
+        }
+    }
+}
